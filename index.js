31383840df5d24d90047ddc8db11b0ab8c8b36ab
Samuel Key <s.key08@gmail.com>
2015-08-24 13:27:57 -0400

    get, post, delete functional for lists

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 var listsController = require("./controllers/lists");
 var contentsController = require("./controllers/contents");
