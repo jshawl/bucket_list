9fc79125768642bfd6892e1a3563d03940a3cb8f
Samuel Key <s.key08@gmail.com>
2015-08-24 11:44:33 -0400

    added in seed data

--- controllers/contents.js
+++ controllers/contents.js
@@ -1,3 +1,14 @@
 var express = require("express");
 var router = express.Router();
 var Content = require("../db/connection").models.Content;
+
+function error(response, message){
+  response.status(500);
+  response.json({error: message})
+}
+
+router.get("/contents", function(req, res){
+  res.send("here's some content")
+});
+
+module.exports = router;
