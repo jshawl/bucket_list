f220aa56edf9a748aea0e59f6e50f0f12cc6d14d
Rachel Warren <raelwa@gmail.com>
2015-08-26 16:36:29 -0400

    prepping for heroku

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,7 @@ var Sequelize = require("sequelize");
 var db_connection = new Sequelize("postgres:///bucket_list");
 var List = db_connection.import("../models/list");
 var Content = db_connection.import("../models/content");
+var sequelize = require('sequelize-heroku').connect();
 
 List.hasMany(Content);
 Content.belongsTo(List);
@@ -14,4 +15,3 @@ module.exports = {
     Content: Content
   }
 }
- 
