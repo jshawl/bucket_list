35cb8487c621cdd91f244f0f0ca455d7ac5e13ec
Rachel Warren <raelwa@gmail.com>
2015-08-27 10:08:01 -0400

    connection.js file for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,31 +1,21 @@
 var Sequelize = require("sequelize");
-var db_connection = new Sequelize("postgres:///bucket_list");
+// var db_connection = new Sequelize("postgres:///bucket_list");
+
+if (process.env.DATABASE_URL) {
+  db_connection = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: true //false
+  });
+} else {
+  db_connection = new Sequelize("postgres:///bucket_list");
+}
+
+
 var List = db_connection.import("../models/list");
 var Content = db_connection.import("../models/content");
 
-var sequelize = require('sequelize-heroku').connect();
 
-if (sequelize)
-{
-    sequelize.authenticate().then( function() {
-        var config = sequelize.connectionManager.config;
-        console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
-
-        sequelize.query('SELECT 1+1 as test').then( function(res) {
-
-            console.log('1+1='+res[0].test);
-
-        });
-
-    }).catch( function(err) {
-        var config = sequelize.connectionManager.config;
-        console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
-    });
-}
-else
-{
-    console.log('No environnement variable found.');
-}
 
 List.hasMany(Content);
 Content.belongsTo(List);
